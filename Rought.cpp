8c1c658d957c6e7f3f97c58306c6749302d87e2e
Uday <udaysharma4600@gmail.com>
2024-02-13 20:16:00 +0530

    13-Feb-2024

--- Rought.cpp
+++ Rought.cpp
@@ -1,10 +1,36 @@
 #include <bits/stdc++.h>
+#include<conio.h>
 using namespace std;
 
 int main() {
 	// your code goes here
-	int a= 2 ,b=3;
-	printf("%d %d",);
-	
+	// int a =10 ,b=20,c=30;
+	// printf("%d",scanf("%d %d",&a,&b));
+
+	// int a =5;
+	// printf("%d %d %d %d",a++,a++,a++,a++);
+
+	// int a =10 ,b=20;
+	// printf("%d %d %d",scanf("%d %d",&a,&b),a,b); // 2 10 20
+
+	// int a,b,c;
+	// // clrscr();
+	// printf("enter the two nos");
+	// scanf("%d %d",&a,&b);
+	// c=a+b;
+	// printf("sum = %d",c);
+
+	// redeclaration error 
+	// int a ;
+	// int a ;
+
+	char c= 1;
+	c=~c;
+	printf("%d",c);
+
+	int a = 2;
+	a =~a;
+	cout<<a<<endl;
+	return 0;
 }
 
