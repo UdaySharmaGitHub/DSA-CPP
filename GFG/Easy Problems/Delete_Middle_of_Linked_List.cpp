a0873c3209259b834a6d3d2fc900d22652ba36e6
Uday <udaysharma4600@gmail.com>
2024-02-19 18:17:32 +0530

    19-Feb-2024

---
+++ GFG/Easy Problems/Delete_Middle_of_Linked_List.cpp
@@ -0,0 +1,126 @@
+/*
+Delete Middle of Linked List
+
+Given a singly linked list, delete middle of the linked list. For example, if given linked list is 1->2->3->4->5 then linked list should be modified to 1->2->4->5.
+If there are even nodes, then there would be two middle nodes, we need to delete the second middle element. For example, if given linked list is 1->2->3->4->5->6 then it should be modified to 1->2->3->5->6.
+If the input linked list has 1 node, then it should return NULL
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+Output: 1 2 4 5
+Example 2:
+
+Input:
+LinkedList: 2->4->6->7->5->1
+Output: 2 4 6 5 1
+Your Task:
+The task is to complete the function deleteMid() which should delete the middle element from the linked list and return the head of the modified linked list. If the linked list is empty then it should return NULL.
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= n <= 105
+1 <= value <= 109
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+        if(head==nullptr){
+            return nullptr;
+        }
+        if(head->next==nullptr){
+            return head->next;
+        }
+        Node* prev  = nullptr , * slow = head , * fast = head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            prev = slow;
+            slow = slow->next;
+            fast =fast->next;
+            if(fast!=nullptr){
+                fast =fast->next;
+            }
+        }
+        prev->next = slow->next;
+        return head;
+        // Your Code Here
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		Solution obj;
+		head = obj.deleteMid(head);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+
+
+// } Driver Code Ends
