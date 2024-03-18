ca3775121aad8ee84eaefe03529926881ebe7d0f
Uday <udaysharma4600@gmail.com>
2024-03-18 17:53:27 +0530

    18-Mar-2024

--- 15_Tree/Binay_Tree/01_Basic_Binary_Tree_intro.cpp
+++ 15_Tree/Binay_Tree/01_Basic_Binary_Tree_intro.cpp
@@ -21,19 +21,19 @@ int main()
 	/*create root*/
 	Node* root = new Node(1);
 		/* following is the tree after above statement
-	1
+	 1
 	/ \
-	NULL NULL
+  NULL NULL
 	*/
 	root->left = new Node(2);
 	root->right = new Node(3);
 
 	/* 2 and 3 become left and right children of 1
-	1
-	/ \
-	2 3
-	/ \ / \
-	NULL NULL NULL NULL
+ 	   1
+ 	  / \
+     2   3
+    / \ / \
+NULL NULL NULL NULL
 	*/
 	root->left->left = new Node(4);
 	/* 4 becomes left child of 2
--- 15_Tree/Binay_Tree/02_Binary_Tree.cpp
+++ 15_Tree/Binay_Tree/02_Binary_Tree.cpp
@@ -16,3 +16,49 @@ public:
 		right = NULL;
 	}
 };
+
+//-----------------------------------------Build Tree ---------------------------------------//
+node *buildTree(node* root){
+	cout<<"Enter the data : "<<endl;
+	int data;
+	cin>>data;
+	root = new node(data);
+	if(data==-1){return nullptr;}
+
+	cout<<"Enter the data for inserting left of "<< data <<endl;
+	root->left = buildTree(root->left);
+	cout<<"Enter the data for inserting right of "<< data <<endl;
+	root->right = buildTree(root->right);
+
+	return root;
+}
+//-----------------------------------------Traversal---------------------------------------//
+//----------------------------Level Order Traversal---------------------------------------//
+void levelOrderTraversal(node* root){
+	queue<node*> q;
+	q.push(root);
+	while(!q.empty()){
+		node * temp = q.front();
+		cout<<temp->data<<" ";
+		q.pop();
+
+		if(temp->left){
+			q.push(temp->left);
+		}
+		if(temp->right){
+			q.push(temp->right);
+		}
+	}
+}
+
+
+int main(){
+	node * root = nullptr;
+	root = buildTree(root);
+	//1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1  
+	//level order
+	cout<<"Printing the Level Order Traversal :"<<endl;
+	levelOrderTraversal(root);
+
+	return 0;
+}
