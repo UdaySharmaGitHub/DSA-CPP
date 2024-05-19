d6def9f61c7110211f8158e39f400f9f733c382d
Uday <udaysharma4600@gmail.com>
2024-05-19 20:47:18 +0530

    19-May-2024

---
+++ GFG/Easy Problems/Find_the_closest_number.cpp
@@ -0,0 +1,88 @@
+/*
+Find the closest number
+Given a sorted array arr[] of positive integers. The task is to find the closest value in the array to the given number k. The array may contain duplicate values.
+
+Note: If the difference with k is the same for two values in the array return the greater value.
+
+Example 1:
+
+Input: 
+n = 4
+k = 4
+arr[] = {1, 3, 6, 7}
+Output: 
+3
+Explanation:
+We have array arr={1, 3, 6, 7} and target is 4. If we look at the absolute difference of target with every element of the array we will get { |1-4|, |3-4|, |6-4|, |7-4| }  = {3, 1, 2, 3}. So, the closest number is 3.
+Example 2:
+
+Input:
+n = 7
+k = 4
+arr[] = {1, 2, 3, 5, 6, 8, 9}
+Output:
+5
+Explanation:
+The absolute difference of 4 is 1 from both 3 and 5. According to the question, we have to return greater value, which is 5.
+Your Task:
+This is a function problem. The input is already taken care of by the driver code. You only need to complete the function findClosest() that takes integers n and k and sorted array arr[] of size n as input parameters and return the closest number in the array to k. 
+
+Expected Time Complexity: O(log(n)).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ n ≤ 106
+1 ≤ k ≤ 109
+1 ≤ arr[i] ≤ 109
+*/
+class Solution{
+    public:
+    int getClosest(int a , int  b , int k){
+        if((k-a) >= ( b - k )) return b;
+        return a;
+    }
+    int findClosest( int n, int target,int arr[]) 
+    { 
+        // Complete the function
+        //left-side case
+    if (target <= arr[0])
+        return arr[0];
+  //right-side case
+    if (target >= arr[n - 1])
+        return arr[n - 1];
+ 
+    // Doing binary search
+    int i = 0, j = n, mid = 0;
+    while (i < j) {
+        mid = (i + j) / 2;
+ 
+        if (arr[mid] == target)
+            return arr[mid];
+ 
+        /* If target is less than array element,
+            then search in left */
+        if (target < arr[mid]) {
+ 
+            // If target is greater than previous
+            // to mid, return closest of two
+            if (mid > 0 && target > arr[mid - 1])
+                return getClosest(arr[mid - 1],
+                                  arr[mid], target);   
+            j = mid;
+        }
+      /* Repeat for left half */
+ 
+        // If target is greater than mid
+        else {
+            if (mid < n - 1 && target < arr[mid + 1])
+                return getClosest(arr[mid],
+                                  arr[mid + 1], target);
+            // update i
+            i = mid + 1; 
+        }
+    }
+ 
+    // Only single element left after search
+    return arr[mid];
+    } 
+};
---
+++ Searching/Binary Search/Problem/Find_the_Highest_number.cpp
@@ -0,0 +1,83 @@
+/*
+Find the Highest number
+Given an integer array a[] of size n, find the highest element of the array. The array will either be strictly increasing or strictly increasing and then strictly decreasing.
+
+Note: a[i] != a[i+1] 
+
+Example 1:
+
+Input:
+11
+1 2 3 4 5 6 5 4 3 2 1
+Output: 
+6
+Explanation: 
+Highest element of array a[] is 6.
+Example 2:
+
+Input:
+5
+1 2 3 4 5
+Output:
+5
+Explanation: 
+Highest element of array a[] is 5.
+Your Task:
+You don't need to read or print anything. Your task is to complete the function findPeakElement() which takes the array a[] as the input parameter and returns the highest element of the array.
+
+Expected Time Complexity: O(log(n))
+Expected Space Complexity: O(1)
+
+Constraints:
+2 <= n <= 106
+1 <= a[i] <= 106
+*/
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        // Code here.
+         int st =0 ,maxa = -INT_MIN;
+        int en =  nums.size() -1 ;
+        int mid = st+ (en-st)/2;
+        while(st<en){
+            if(nums[mid]<nums[mid+1]){
+                st =mid+1;
+            }
+            else{
+                en =mid;
+            }
+            maxa = max(maxa,nums[mid]);
+             mid = st+ (en-st)/2;
+        }
+        return nums[st];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
