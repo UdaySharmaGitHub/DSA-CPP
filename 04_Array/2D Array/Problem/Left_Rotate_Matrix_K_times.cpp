7fc15d672091c7e0aa7ad73263603fc4f1a065a8
Uday <udaysharma4600@gmail.com>
2024-06-25 23:07:29 +0530

    25-Jun-2024

---
+++ 04_Array/2D Array/Problem/Left_Rotate_Matrix_K_times.cpp
@@ -0,0 +1,76 @@
+/*
+Left Rotate Matrix K times
+You are given an integer k and matrix mat. Rotate the elements of the given matrix to the left k times and return the resulting matrix.
+
+Examples:
+Input: k=1, mat=[[1,2,3],[4,5,6],[7,8,9]]
+Output:
+2 3 1
+5 6 4
+8 9 7
+Explanation: Rotate the matrix by one
+1 2 3       2 3 1
+4 5 6  =>  5 6 4
+7 8 9       8 9 7
+Input: k=2, mat=[[1,2,3],[4,5,6],[7,8,9]]
+Output:
+3 1 2
+6 4 5
+9 7 8
+Explanation: After rotating the matrix looks like
+1 2 3       2 3 1       3 1 2
+4 5 6  =>  5 6 4  =>   6 4 5
+7 8 9       8 9 7       9 7 8
+Expected Time Complexity: O(n*m)
+Expected Auxillary Space: O(n*m)
+Constraints:
+1<= mat.size, mat[0].size, mat[i][j] <=1000
+1<=k<=10000
+*/
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> rotateMatrix(int k, vector<vector<int>> mat) {
+        // code here
+        k%=mat[0].size();
+        for(int i =0;i<mat.size();i++){
+            reverse(mat[i].begin(),mat[i].begin()+k);
+            reverse(mat[i].begin()+k,mat[i].end());
+            reverse(mat[i].begin(),mat[i].end());
+        }
+        return mat;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        vector<vector<int>> mat(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> mat[i][j];
+        Solution ob;
+        vector<vector<int>> ans = ob.rotateMatrix(k, mat);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                cout << ans[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+
+// } Driver Code Ends
