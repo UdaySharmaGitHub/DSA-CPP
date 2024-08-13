89870b5cea7d545f08c79dcd159cbfa3718a3bc7
Uday <udaysharma4600@gmail.com>
2024-08-13 22:27:34 +0530

    POTD 13-Aug-2024

--- GFG/Easy Problems/Square_root_of_a_number.cpp
+++ GFG/Easy Problems/Square_root_of_a_number.cpp
@@ -23,11 +23,6 @@ Constraints:
 1 ≤ x ≤ 107
 */
 //{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-// } Driver Code Ends
-// Function to find square root
-// x: element to find square root
 class Solution{
   public:
     long long int floorSqrt(long long int x) 
@@ -55,18 +50,3 @@ class Solution{
         return ans;
     }
 };
-//{ Driver Code Starts.
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		long long n;
-		cin>>n;
-		Solution obj;
-		cout << obj.floorSqrt(n) << endl;
-	}
-    return 0;   
-}
-// } Driver Code Ends
