79dca70fec947c4e2837e83cb0c37260ecc5dd94
Uday <udaysharma4600@gmail.com>
2024-08-15 19:23:08 +0530

    POTD 15-Aug-2024

--- 12_Linked_List/SLL_Problems/Medium/Add_1_to_a_number_represented_as_linked_list.cpp
+++ 12_Linked_List/SLL_Problems/Medium/Add_1_to_a_number_represented_as_linked_list.cpp
@@ -1,55 +1,23 @@
 /*
 Add 1 to a number represented as linked list
 A number N is represented in Linked List such that each digit corresponds to a node in linked list. You need to add 1 to it.
-
 Example 1:
-
 Input:
 LinkedList: 4->5->6
 Output: 457
 Explanation: 4->5->6 represents 456 and when 1 is added it becomes 457. 
 Example 2:
-
 Input:
 LinkedList: 1->2->3
 Output: 124 
 Your Task:
 Your task is to complete the function addOne() which takes the head of the linked list as the only argument and returns the head of the modified linked list. The driver code prints the number.
 Note: The head represents the left-most digit of the number.
-
 Expected Time Complexity: O(N).
 Expected Auxiliary Space: O(1).
-
 Constraints:
 1 <= N <= 1021
 */
-//{ Driver Code Starts
-//Initial template for C++
-
-#include <bits/stdc++.h> 
-using namespace std; 
-
-struct Node
-{
-    int data;
-    struct Node* next;
-    
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-};
-
-void printList(Node* node) 
-{ 
-    while (node != NULL) { 
-        cout << node->data%10; 
-        node = node->next; 
-    }  
-    cout<<"\n";
-} 
-
-
 // } Driver Code Ends
 //User function template for C++
 
@@ -67,7 +35,43 @@ struct Node
 };
 
 */
-
+//-------------------------------------MOST OPTIMIZED APPRAOCH ---------------------------------------//
+class Solution {
+  public:
+    Node* reverse(Node* head ){
+      Node*  prev  = nullptr , *curr = head , *nxt = nullptr;
+      while(curr){
+          nxt = curr->next;
+          curr->next  = prev;
+          prev = curr;
+          curr =nxt;
+      }
+      return prev;
+  }
+    Node* addOne(Node* head) {
+        // Your Code here
+        // return head of list after adding one
+        int carry = 1;
+        if(!head ) return head;
+        head = reverse(head);
+        Node* curr = head;
+        while(curr){
+            int sum  =curr->data+ carry;
+            carry = sum/10;
+            curr->data = sum%10;
+            if(carry==0) break;
+            if(!curr->next && carry > 0){
+                curr->next = new Node(carry);
+                carry = 0;
+            }
+            curr = curr->next;
+        }
+        head = reverse(head);
+        return head;
+        
+    }
+};
+//-------------------------------------Long APPRAOCH ---------------------------------------//
 class Solution
 {
     private:
@@ -122,30 +126,3 @@ class Solution
     return ansHead;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() 
-{ 
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        string s;
-        cin>>s;
-        
-        Node* head = new Node( s[0]-'0' );
-        Node* tail = head;
-        for(int i=1; i<s.size(); i++)
-        {
-            tail->next = new Node( s[i]-'0' );
-            tail = tail->next;
-        }
-        Solution ob;
-        head = ob.addOne(head);
-        printList(head); 
-    }
-    return 0; 
-} 
-
-// } Driver Code Ends
