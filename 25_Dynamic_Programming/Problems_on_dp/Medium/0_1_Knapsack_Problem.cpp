b19e3b1061be90c09599da9d6e7341f5d4007aa5
Uday <udaysharma4600@gmail.com>
2024-08-24 22:22:50 +0530

    POTD 24-Aug-2024

---
+++ 25_Dynamic_Programming/Problems_on_dp/Medium/0_1_Knapsack_Problem.cpp
@@ -0,0 +1,46 @@
+/*
+0 - 1 Knapsack Problem
+You are given weights and values of items, and put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+In other words, given two integer arrays val and wt which represent values and weights associated with items respectively. Also given an integer W which represents knapsack capacity, find out the maximum sum values subset of val[] such that the sum of the weights of the corresponding subset is smaller than or equal to W. You cannot break an item, either pick the complete item or don't pick it (0-1 property).
+
+Examples :
+
+Input: W = 4, val[] = {1,2,3}, wt[] = {4,5,1}
+Output: 3
+Explanation: Choose the last item that weighs 1 unit and holds a value of 3. 
+Input: W = 3, val[] = {1,2,3}, wt[] = {4,5,6}
+Output: 0
+Explanation: Every item has a weight exceeding the knapsack's capacity (3).
+Expected Time Complexity: O(N*W).
+Expected Auxiliary Space: O(N*W)
+
+Constraints:
+2 ≤ N ≤ 1000
+1 ≤ W ≤ 1000
+1 ≤ wt[i] ≤ 1000
+1 ≤ val[i] ≤ 1000
+*/
+class Solution {
+  public:
+    // Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, vector<int>& wt, vector<int>& val) {
+        // Your code here
+         int i, w ,n = wt.size();
+    vector<vector<int> > K(n + 1, vector<int>(W + 1));
+
+    // Build table K[][] in bottom up manner
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1]
+                                  + K[i - 1][w - wt[i - 1]],
+                              K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    return K[n][W];
+    }
+};
