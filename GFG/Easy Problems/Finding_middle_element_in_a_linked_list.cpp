c370860ac7f8b7c3d37e482b4415def2842450da
Uday <udaysharma4600@gmail.com>
2024-09-13 19:11:43 +0530

    POTD 12-Sep-2024

--- GFG/Easy Problems/Finding_middle_element_in_a_linked_list.cpp
+++ GFG/Easy Problems/Finding_middle_element_in_a_linked_list.cpp
@@ -4,16 +4,13 @@ The task is to find the middle of the linked list. For example, if the linked li
 1-> 2->3->4->5, then the middle node of the list is 3.
 If there are two middle nodes(in case, when N is even), print the second middle element.
 For example, if the linked list given is 1->2->3->4->5->6, then the middle node of the list is 4.
-
 Example 1:
-
 Input:
 LinkedList: 1->2->3->4->5
 Output: 3 
 Explanation: 
 Middle of linked list is 3.
 Example 2: 
-
 Input:
 LinkedList: 2->4->6->7->5->1
 Output: 7 
@@ -21,40 +18,12 @@ Explanation:
 Middle of linked list is 7.
 Your Task:
 The task is to complete the function getMiddle() which takes a head reference as the only argument and should return the data at the middle node of the linked list.
-
 Expected Time Complexity: O(N).
 Expected Auxiliary Space: O(1).
-
 Constraints:
 1 <= N <= 5000
 */
-//{ Driver Code Starts
-//Initial template for C++
 
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
-    int data;
-    struct Node* next;
-    
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-};
-void printList(Node* node) 
-{ 
-    while (node != NULL) { 
-        cout << node->data <<" "; 
-        node = node->next; 
-    }  
-    cout<<"\n";
-} 
-
-
-// } Driver Code Ends
 /* Link list Node 
 struct Node {
     int data;
@@ -66,6 +35,22 @@ struct Node {
     }
     
 }; */
+class Solution {
+  public:
+    /* Should return data of middle node. If linked list is empty, then -1 */
+    int getMiddle(Node* head) {
+        // code here
+        if(!head || !head->next) return head->data;
+        Node* slow = head  , *fast = head->next;
+        while(fast){
+            slow = slow ->next;
+            fast = fast->next;
+            if(fast) fast =fast->next;
+        }        
+        return slow->data;
+    }
+};
+
 class Solution{
     public:
     /* Should return data of middle node. If linked list is empty, then  -1*/
@@ -91,32 +76,3 @@ class Solution{
         return temp->data;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    //code
-    int t;
-    cin>>t;
-    while(t--){
-        int N;
-        cin>>N;
-        int data;
-        cin>>data;
-        struct Node *head = new Node(data);
-        struct Node *tail = head;
-        for (int i = 0; i < N-1; ++i)
-        {
-            cin>>data;
-            tail->next = new Node(data);
-            tail = tail->next;
-        }
-        
-        Solution ob;
-        cout << ob.getMiddle(head) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
